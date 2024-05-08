2a7e3ece22148e4f14e0c54b0639157995d8e339
Rafael Montenegro <71800257+rafaelcampael@users.noreply.github.com>
2024-05-08 20:19:07 -0300

    Projeto estacionamento
    O projeto do estacionamento cadastra um veículo e realiza o cálculo do preço a ser pago no final.
    Projeto feito com .Net 8/ C#

---
+++ Estacionamento/DesafioFundamentos/Models/Estacionamento.cs
@@ -0,0 +1,75 @@
+namespace DesafioFundamentos.Models
+{
+    public class Estacionamento
+    {
+        private decimal precoInicial = 0;
+        private decimal precoPorHora = 0;
+        private List<string> veiculos = new List<string>();
+
+        public Estacionamento(decimal precoInicial, decimal precoPorHora)
+        {
+            this.precoInicial = precoInicial;
+            this.precoPorHora = precoPorHora; 
+        } 
+
+        public void AdicionarVeiculo()
+        {
+            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
+            Console.WriteLine("Digite a placa do veículo para estacionar:");
+            veiculos.Add(Console.ReadLine());
+        }
+            
+        public void RemoverVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veículo para remover:");
+
+            // Pedir para o usuário digitar a placa e armazenar na variável placa
+            string placa = Console.ReadLine();
+            
+            
+            // Verifica se o veículo existe
+            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            {
+                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+
+                //Pede para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
+                int horas = Convert.ToInt32(Console.ReadLine());
+                //Realiza o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
+                decimal valorTotal = 0; 
+                valorTotal = horas * precoPorHora + precoInicial;
+                
+                //Remove a placa digitada da lista de veículos
+                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+                veiculos.Remove(placa);
+            }
+            else
+            {
+                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+            }
+        }
+                
+
+
+        public void ListarVeiculos()
+        {
+            // Verifica se há veículos no estacionamento
+            if (veiculos.Any())
+            {
+                Console.WriteLine("Os veículos estacionados são:");
+
+                //Realiza um laço de repetição, exibindo os veículos estacionados
+                foreach (var cars in veiculos)
+                {
+                    Console.WriteLine(cars);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não há veículos estacionados.");
+            }
+        }
+    }
+}
+            
+
+            
